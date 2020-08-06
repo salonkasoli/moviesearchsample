c65839b217bf920d4767f8c2036f39beb9828f9d
salonkasoli <salonkasoli@gmail.com>
2020-08-06 18:11:04 +0700

    уничтожил собственный DI

--- app/src/main/java/com/github/salonkasoli/moviesearchsample/App.kt
+++ app/src/main/java/com/github/salonkasoli/moviesearchsample/App.kt
@@ -2,19 +2,10 @@ package com.github.salonkasoli.moviesearchsample
 
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.github.salonkasoli.moviesearchsample.auth.SessionIdCache
-import com.github.salonkasoli.moviesearchsample.detail.MovieDetailCache
 import com.github.salonkasoli.moviesearchsample.di.AppComponent
 import com.github.salonkasoli.moviesearchsample.di.DaggerAppComponent
 import com.github.salonkasoli.moviesearchsample.di.module.AppModule
 import com.github.salonkasoli.moviesearchsample.di.module.IOModule
-import com.github.salonkasoli.moviesearchsample.search.MovieListCache
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.*
-import kotlin.collections.HashMap
-import kotlin.collections.get
-import kotlin.collections.set
 
 class App : Application() {
 
@@ -24,20 +15,6 @@ class App : Application() {
         super.onCreate()
         instance = this
         Fresco.initialize(this)
-
-        put(MovieListCache())
-        put(MovieDetailCache())
-        put(SessionIdCache(this))
-
-        val a = ArrayList<Int>()
-        a.remove(1)
-        put(
-            Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl(Const.MOVIE_DB_URL)
-                .build()
-        )
-
         appComponent = DaggerAppComponent.builder()
             .iOModule(IOModule())
             .appModule(AppModule(this))
@@ -46,16 +23,5 @@ class App : Application() {
 
     companion object {
         lateinit var instance: App
-
-        // Вместо Dagger2 здесь живет небольшой контейнер зависимостей.
-        private val deps = HashMap<Class<out Any>, Any>()
-
-        fun <T : Any> put(any: T) {
-            deps[any::class.java] = any
-        }
-
-        fun <T> get(clazz: Class<T>): T {
-            return deps[clazz] as T
-        }
     }
 }
